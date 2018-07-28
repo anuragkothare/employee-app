27ee17763024f400273d3d72546424b8158eb79b
anuragkothare <anuragkothare@outlook.com>
2018-07-29 03:32:45 +0530

    Fixed server routes for angular

--- server.js
+++ server.js
@@ -30,6 +30,11 @@ app.use(express.static(distDir));
 
 // Use Routes
 app.use('/api/employees', employee)
+
+app.get('*', function(req, res) {
+    res.sendfile('./client/dist/index.html')
+  })
+
 const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running on port ${port}`) )
